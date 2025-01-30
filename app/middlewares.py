9c55e30d881f56f09a1409dc25b2cfeac9e369a7
orriginalo <originalo1717@gmail.com>
2025-01-30 20:46:18 +0400

    msglogger middleware reworked

--- app/middlewares.py
+++ app/middlewares.py
@@ -89,7 +89,6 @@ class MsgLoggerMiddleware(BaseException):
         data: Dict[str, Any]
     ) -> Any:
         
-        
         user = await get_user_by_id(event.from_user.id)
         print("User is none?")
         if user is None:
@@ -103,24 +102,44 @@ class MsgLoggerMiddleware(BaseException):
                 group_id=None,
                 is_leader=False
             )
-                
         else:
             if user["role"] == 0:
-                return
-        await update_user(event.from_user.id, username=event.from_user.username, firstname=event.from_user.first_name, lastname=event.from_user.last_name)
+                return  # Прерываем выполнение, если роль 0
         
+        # Обновляем данные пользователя в БД
+        await update_user(
+            event.from_user.id,
+            username=event.from_user.username,
+            firstname=event.from_user.first_name,
+            lastname=event.from_user.last_name
+        )
+        
+        # Добавляем пользователя в data
         data["user"] = user
         msg = ""
-        if hasattr(event, "data"):
-            msg = event.data
-        if hasattr(event, "text"):
-            msg = event.text
-        user_name = f"{event.from_user.first_name if event.from_user.first_name else ''}{(" " + event.from_user.last_name) if event.from_user.last_name else ''}"
+
+        # Определяем имя пользователя
+        user_name = f"{event.from_user.first_name or ''}{' ' + event.from_user.last_name if event.from_user.last_name else ''}"
+
         try:
-            if event.content_type == "text":
-                logger.info(f'[{user_name}] - "{msg}"')
-            else:
-                logger.info(f'[{user_name}] - "some <{event.content_type}>"')
+            if isinstance(event, Message):
+                msg = event.text if event.content_type == "text" else f"some <{event.content_type}>"
+                logger.info(f'Message  - [{user_name}] - "{msg}"')
+
+            elif isinstance(event, CallbackQuery):
+                data["callback_data"] = event.data  # Не перезаписываем весь `data`
+                msg = ""
+                
+                if event.message.reply_markup and event.message.reply_markup.inline_keyboard:
+                    for line in event.message.reply_markup.inline_keyboard:
+                        for button in line:
+                            if button.callback_data == event.data:
+                                msg = button.text
+                                break
+                
+                logger.info(f'Callback - [{user_name}] - "{msg}" : {event.data}')
+
         except Exception as e:
-            print(e)
-        return await handler(event, data)
+            print(f"Ошибка в логировании: {e}")
+
+        return await handler(event, data)  # `data` остаётся словарём!
