4f697bd54425f8b5f7d6a9c2fd9364b02ded7771
orriginalo <originalo1717@gmail.com>
2025-02-12 19:56:04 +0400

    hotfix

--- app/services/utils.py
+++ app/services/utils.py
@@ -6,13 +6,13 @@ async def send_media(chat_id: int, bot: Bot, medias: list[MediaSchema], text: st
   media_list = []
   for media in medias:
     if media.media_type == "photo":
-      media_list.append(InputMediaPhoto(media=media.media))
+      media_list.append(InputMediaPhoto(media=media.media_id))
     elif media.media_type == "video":
-      media_list.append(InputMediaVideo(media=media.media))
+      media_list.append(InputMediaVideo(media=media.media_id))
     elif media.media_type == "audio":
-      media_list.append(InputMediaAudio(media=media.media))
+      media_list.append(InputMediaAudio(media=media.media_id))
     elif media.media_type == "document":
-      media_list.append(InputMediaDocument(media=media.media))
+      media_list.append(InputMediaDocument(media=media.media_id))
     else:
       media_list.append(media)
       
